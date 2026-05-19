c738890516e71d96b8ed28c420049823f84ed6f2
nscarpinatodev <nick@stuffnicksays.com>
2026-05-19 18:59:29 -0500

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   .claude/settings.local.json
    	modified:   .gitignore
    	modified:   module.json
    	modified:   package.json
    	deleted:    pnpm-lock.yaml
    	modified:   scripts/inject-id.js

--- scripts/inject-id.js
+++ scripts/inject-id.js
@@ -7,6 +7,7 @@ const filePath = path.resolve(__dirname, "../dist/module.json");
 let moduleJson = fs.readFileSync(filePath, "utf8");
 
 moduleJson = moduleJson.replace(/ID_PLACEHOLDER/g, pkg.name);
+moduleJson = moduleJson.replace(/VERSION_TAG/g, pkg.version);
 
 fs.writeFileSync(filePath, moduleJson);
-console.log(`Replaced ID_PLACEHOLDER with "${pkg.name}"`);
+console.log(`Injected id="${pkg.name}" version="${pkg.version}"`);
